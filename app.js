a960af153dffe2b9e8d9bfae26e17569f6209e02
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-06-27 20:15:00 +0300

    some changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const port = 3000;
 const app = express();
