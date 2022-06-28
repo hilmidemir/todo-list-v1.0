2e30847b4e284debaed4561a4899d0b3afbf84e3
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-06-28 23:03:10 +0300

    some changes

--- app.js
+++ app.js
@@ -5,43 +5,29 @@ const ejs = require("ejs");
 const port = 3000;
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
-  var week = [ "Sunday", "Monday", "Tuesday", "Wendesday", "Thursday", "Friday", "Saturday"];
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = week[0];
-      break;
-    case 1:
-      day = week[1];
-      break;
-    case 2:
-      day = week[2];
-      break;
-    case 3:
-      day = week[3];
-      break;
-    case 4:
-      day = week[4];
-      break;
-    case 5:
-      day = week[5];
-      break;
-    case 6:
-      day = week[6];
-      break;
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
-
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
+  var day = today.toLocaleDateString("en-us", options);
+
   res.render("list", {kindOfDay: day});
 });
+
+app.post("/", function(req, res) {
+  var item = req.body.newItem;
+  console.log(item);
+});
+
 
 
 app.listen(port, function() {
