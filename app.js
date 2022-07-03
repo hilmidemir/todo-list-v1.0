e0d29fe8cd895045005050cd218be74000dd8f62
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-03 15:44:56 +0300

    adding about.ejs and fixing some bugs

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const port = 3000;
 const app = express();
 
 var items = ["Buy Food", "Cook Food", "Eat Food"];
-var workItems = ""; 
+var workItems = [];
 
+//Import Body parser assigments and express folder usages
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//Import ejs for usage
 app.set('view engine', 'ejs');
 
+// seting up home route and rendered something
 app.get("/", function(req, res) {
 
   var today = new Date();
@@ -30,28 +33,30 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
 
   let item = req.body.newItem;
-
+  console.log(item);
   if(req.body.list === "Work") {
     workItems.push(item);
-    res.redirect("/work")
+    res.redirect("/work");
   }else{
     items.push(item);
     res.redirect("/");
   }
-  
+
 });
 
 app.get("/work", function(req, res) {
   res.render("list", {listTitle: "Work List", newListItems: workItems})
 });
 
-app.post("/work", function() {
+ app.post("/work", function() {
   let item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
-
+app.get("/about", function(req, res) {
+  res.render("about");
+});
 
 app.listen(port, function() {
   console.log("Server started on port " + port + "...");
