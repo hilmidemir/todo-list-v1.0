7c7578e0b3b2dc054cb2a6bf1f2b701634d29515
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-03 22:40:33 +0300

    s

--- date.js
+++ date.js
@@ -1,33 +1,24 @@
 //jshint esversion:6
 
 // That formul export our function's return and it allow us use in the main page
-module.exports.getDate = getDate;
+getDate = getDate;
 
 // The function what we wanna do
-function getDate() {
-
+module.exports.getDate = function () {
   let today = new Date();
-
   let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   }
   let day = today.toLocaleDateString("en-us", options);
-
-  return day;
 }
 
 module.exports.getDay = getDay;
-
 function getDay() {
-
   let today = new Date();
-
   let options = {
     weekday: "long"
   }
   let day = today.toLocaleDateString("en-us", options);
-
-  return day;
 }
