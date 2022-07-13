d78658b3c509d3bd5ea8db2e037dd62bd4ae812e
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-13 17:12:30 +0300

    save in database your todo's

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![Deploy to Cyclic](https://deploy.cyclic.app/button.svg)](https://deploy.cyclic.app/)
 
-todo List Website's master branch
+todo List Website's version 2.0
--- app.js
+++ app.js
@@ -31,9 +31,6 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-
-
-
 app.get("/", function(req, res) {
   Item.find({}, function(err, foundItems) {
     if (foundItems.length === 0) {
@@ -54,15 +51,13 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item ({
+    name: itemName
+  });
+  item.save();
+  res.redirect("/");
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
 });
 
 app.get("/work", function(req,res){
